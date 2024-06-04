12989173c1ce96d36b0a9c81e50713d44c588867
Pierre Ayoub <pierre.ayoub.pro@tutanota.com>
2024-06-04 18:01:30 +0200

    Re-organize functions order in cli.py

--- src/soapyrx/cli.py
+++ src/soapyrx/cli.py
@@ -71,6 +71,66 @@ def cli(config, dir, log, loglevel):
         l.LOGGER.error("Configuration file not found: {}".format(path.abspath(config)))
 
 @cli.command()
+def discover():
+    """Discover SDRs.
+
+    Discover connected SDRs and print capabilities.
+
+    """
+    results = SoapySDR.Device.enumerate()
+    for idx, result in enumerate(results):
+        l.LOGGER.info("{}".format(result))
+        l.LOGGER.info("Index: {}".format(idx))
+        sdr = SoapySDR.Device(result)
+        # Query device info.
+        l.LOGGER.info("Antennas: {}".format(sdr.listAntennas(SoapySDR.SOAPY_SDR_RX, 0)))
+        l.LOGGER.info("Gains: {}".format(sdr.listGains(SoapySDR.SOAPY_SDR_RX, 0)))
+        freqRanges = sdr.getFrequencyRange(SoapySDR.SOAPY_SDR_RX, 0)
+        for freqRange in freqRanges:
+            l.LOGGER.info("Frenquency range: {}".format(freqRange))
+    if not results:
+        l.LOGGER.error("No detected SDR!"); exit(1)
+
+@cli.command()
+@click.argument("freq", type=float)
+@click.argument("samp", type=float)
+@click.option("--duration", type=float, default=1, help="Duration of the recording [s].")
+@click.option("--gain", type=int, default=0, help="Gain for the SDR [dB].")
+@click.option("--save-sig", default="", help="If set to a file path, save the recorded signal as .npy file.")
+@click.option("--save-plot", default="", help="If set to a file path, save the plot to this path.")
+@click.option("--plot/--no-plot", "plot_flag", default=True, help="Plot the recorded signal.")
+@click.option("--cut/--no-cut", "cut_flag", default=True, help="Cut the recorded signal.")
+def record(freq, samp, duration, gain, save_sig, save_plot, plot_flag, cut_flag):
+    """Record a signal.
+
+    It will automatically use the first found radio. FREQ is the center
+    frequency (e.g., 2.4e9). SAMP is the sampling rate (e.g., 4e6).
+
+    """
+    sig = helpers.record(freq=freq, samp=samp, duration=duration, gain=gain, save_sig=save_sig, save_plot=save_plot, plot_flag=plot_flag, cut_flag=cut_flag, dir=DIR)
+    helpers.plot(sig, samp=samp, freq=freq, cut_flag=cut_flag, plot_flag=plot_flag, save_sig=save_sig, save_plot=save_plot, title=save_sig)
+
+@cli.command()
+@click.argument("file", type=click.Path())
+@click.option("--cut/--no-cut", "cut_flag", default=False, help="Cut the plotted signal.")
+@click.option("--save-sig", default="", help="If set to a file path, save the plotted signal as .npy file.")
+@click.option("--save-plot", default="", help="If set to a file path, save the plot to this path.")
+@click.option("--freq", type=float, default=None, help="Center frequency of the recording (plot feature).")
+@click.option("--samp", type=float, default=None, help="Sampling rate of the recording (plot feature).")
+def plot(file, cut_flag, save_sig, save_plot, freq, samp):
+    """Plot a signal.
+
+    FILE is the path to the file.
+
+    """
+    # Signal loading block.
+    try:
+        sig = np.load(file)
+    except Exception as e:
+        l.LOGGER.critical("Cannot load signal from disk: {}".format(e)); exit(1)
+    helpers.plot(sig, samp=samp, freq=freq, cut_flag=cut_flag, plot_flag=True, save_sig=save_sig, save_plot=save_plot, title=file)
+
+@cli.command()
 @click.argument("freq", type=float)
 @click.argument("samp_rate", type=float)
 @click.option("--duration", type=float, default=0.5, help="Duration of the recording.")
@@ -110,45 +170,6 @@ def server_stop():
 
     """
     core.SoapyClient().quit()
-
-@cli.command()
-@click.argument("file", type=click.Path())
-@click.option("--cut/--no-cut", "cut_flag", default=False, help="Cut the plotted signal.")
-@click.option("--save-sig", default="", help="If set to a file path, save the plotted signal as .npy file.")
-@click.option("--save-plot", default="", help="If set to a file path, save the plot to this path.")
-@click.option("--freq", type=float, default=None, help="Center frequency of the recording (plot feature).")
-@click.option("--samp", type=float, default=None, help="Sampling rate of the recording (plot feature).")
-def plot(file, cut_flag, save_sig, save_plot, freq, samp):
-    """Plot a signal.
-
-    FILE is the path to the file.
-
-    """
-    # Signal loading block.
-    try:
-        sig = np.load(file)
-    except Exception as e:
-        l.LOGGER.critical("Cannot load signal from disk: {}".format(e)); exit(1)
-    helpers.plot(sig, samp=samp, freq=freq, cut_flag=cut_flag, plot_flag=True, save_sig=save_sig, save_plot=save_plot, title=file)
-
-@cli.command()
-@click.argument("freq", type=float)
-@click.argument("samp", type=float)
-@click.option("--duration", type=float, default=1, help="Duration of the recording [s].")
-@click.option("--gain", type=int, default=0, help="Gain for the SDR [dB].")
-@click.option("--save-sig", default="", help="If set to a file path, save the recorded signal as .npy file.")
-@click.option("--save-plot", default="", help="If set to a file path, save the plot to this path.")
-@click.option("--plot/--no-plot", "plot_flag", default=True, help="Plot the recorded signal.")
-@click.option("--cut/--no-cut", "cut_flag", default=True, help="Cut the recorded signal.")
-def record(freq, samp, duration, gain, save_sig, save_plot, plot_flag, cut_flag):
-    """Record a signal.
-
-    It will automatically use the first found radio. FREQ is the center
-    frequency (e.g., 2.4e9). SAMP is the sampling rate (e.g., 4e6).
-
-    """
-    sig = helpers.record(freq=freq, samp=samp, duration=duration, gain=gain, save_sig=save_sig, save_plot=save_plot, plot_flag=plot_flag, cut_flag=cut_flag, dir=DIR)
-    helpers.plot(sig, samp=samp, freq=freq, cut_flag=cut_flag, plot_flag=plot_flag, save_sig=save_sig, save_plot=save_plot, title=save_sig)
     
 @cli.command()
 @click.option("--save", default="", help="If set to a file path, save the recorded signal as .npy file.")
@@ -176,27 +197,6 @@ def client(save, amplitude, phase, plot_flag):
     # Save the signal as requested.
     if save != "":
          np.save(save, sig)
-
-@cli.command()
-def discover():
-    """Discover SDRs.
-
-    Discover connected SDRs and print capabilities.
-
-    """
-    results = SoapySDR.Device.enumerate()
-    for idx, result in enumerate(results):
-        l.LOGGER.info("{}".format(result))
-        l.LOGGER.info("Index: {}".format(idx))
-        sdr = SoapySDR.Device(result)
-        # Query device info.
-        l.LOGGER.info("Antennas: {}".format(sdr.listAntennas(SoapySDR.SOAPY_SDR_RX, 0)))
-        l.LOGGER.info("Gains: {}".format(sdr.listGains(SoapySDR.SOAPY_SDR_RX, 0)))
-        freqRanges = sdr.getFrequencyRange(SoapySDR.SOAPY_SDR_RX, 0)
-        for freqRange in freqRanges:
-            l.LOGGER.info("Frenquency range: {}".format(freqRange))
-    if not results:
-        l.LOGGER.error("No detected SDR!"); exit(1)
     
 if __name__ == "__main__":
     cli()
